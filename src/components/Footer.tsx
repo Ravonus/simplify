dae42b594e48d802199c12c56aad72878b693272
Chad <chadkoslovsky@gmail.com>
2023-08-14 06:45:57 -0600

    Fixed footer date

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -47,7 +47,7 @@ export default function Footer() {
         </div>
         <div className="mt-8 md:order-1 md:mt-0">
           <p className="text-center text-xs leading-5 text-gray-500">
-            &copy; 2024{" "}
+            &copy; 2023{" "}
             {
               <a
                 className="text-blue-500 transition-colors duration-700 ease-in-out hover:text-blue-900"
